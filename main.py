6ca1873f9a7f8c41d3c34728642619326ee4ec9b
carmo-lucas <msclucaslima@gmail.com>
2022-06-22 16:27:57 -0300

    Tentando arrumar as funções

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ import time
 # Grupo 1 é a parte necessária
 validacao_url = u"(http\:\/\/www\.fazenda\.pr\.gov\.br\/nfce\/qrcode\?p\=\d{44})(\|\d){3}\|\w{40}"
 
-
 url = "http://www.fazenda.pr.gov.br/nfce/qrcode?p=41220576189406002250651160003910901081932515|2|1|1|8172512119B92F5F4BBCF8D1E4FF617D304FD593"
 
 def importar_lista_url_nfce():
@@ -38,37 +37,10 @@ def solicitar_pagina_nfce(url):
     page = BeautifulSoup(r.content, 'html.parser')
     return page
 
+
 # Função para extrair metadados conta despesa, data da transação, número da nfce e cnpj
 def achar_metadados(page):
     
-    def achar_conta_despesa(page):
-        conta_despesa = page.find('div', id="u20").text
-        print(conta_despesa)
-        return conta_despesa
-
-    def achar_cnpj(page):
-        cnpj = page.find_all('div', class_='text')[0].text
-        cnpj = re.search(r"\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}", cnpj).group(0)
-        print(cnpj)
-        return cnpj
-
-    def achar_data_transacao(page):
-        data_transacao = page.find('li').text
-        data_transacao = re.search(r"((\d+\/){2}\d{4}\s(\d{2}:){2}\d{2})", data_transacao).group(0)
-        print(data_transacao)
-        return data_transacao
-
-    def achar_num_nfce(page):
-        numero_nfce = page.find('li').text
-        numero_nfce = re.search(r"Número: (\d+)", numero_nfce).group(1)
-        print(numero_nfce)
-        return numero_nfce
-
-    def achar_chave_acesso(page):
-        chave_acesso = page.find
-        chave_acesso = re.search(r"\d{11}", chave_acesso)
-        print(chave_acesso)
-        return chave_acesso
 
     cd    = achar_conta_despesa(page)
     cnpj  = achar_cnpj(page)
@@ -86,27 +58,44 @@ def achar_metadados(page):
 
     return meta
 
+def achar_conta_despesa(page):
+    conta_despesa = page.find('div', id="u20").text
+    print(conta_despesa)
+    return conta_despesa
+
+def achar_cnpj(page):
+    cnpj = page.find_all('div', class_='text')[0].text
+    cnpj = re.search(r"\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}", cnpj).group(0)
+    print(cnpj)
+    return cnpj
+
+def achar_data_transacao(page):
+    data_transacao = page.find('li').text
+    data_transacao = re.search(r"((\d+\/){2}\d{4}\s(\d{2}:){2}\d{2})", data_transacao).group(0)
+    print(data_transacao)
+    return data_transacao
+
+def achar_num_nfce(page):
+    numero_nfce = page.find('li').text
+    numero_nfce = re.search(r"Número: (\d+)", numero_nfce).group(1)
+    print(numero_nfce)
+    return numero_nfce
+
+def achar_chave_acesso(page):
+    chave_acesso = page.find("span", {"class": "chave"})
+    chave_acesso = re.search(r"\d?+", chave_acesso)
+    print(chave_acesso)
+    return chave_acesso
+
+
 page = solicitar_pagina_nfce(url)
-
-meta = achar_info_cabecalho(page)
-
-
-
-
-
-
-
-
-
-
+chave = achar_chave_acesso(page)
 
 
 def ler_tabela_suja(url):
     tbl_suja = pd.read_html(url, encoding='utf8')
     tbl_suja = pd.concat(tbl_suja)
     return tbl_suja
-
-
 
 # Funções para limpar os dados da tabela extraída da página
 
@@ -118,8 +107,6 @@ def remover_texto_indesejado(tbl):
     tbl[0] = tbl[0].str.replace(texto_indesejado, "")
     return tbl
 
-
-
 def separar_colunas(tbl):
     # Separa os valores em colunas
     tbl[['nome', 'qtde', 'unidade', 'vl_unit']] = tbl[0].str.split(pat='[ ]{2,}', expand=True)
@@ -127,8 +114,6 @@ def separar_colunas(tbl):
     # pat='[ ]{2,}' significa dois ou mais espaços
     tbl = tbl.drop(columns=[0, 1])
     return tbl
-
-
 
 def formatar_valores_numericos(tbl):
     # Troca vírgulas por pontos para futura conversão para valores numéricos
