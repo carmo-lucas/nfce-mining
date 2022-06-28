6dcec9b2ea52d914211461582c365875239958ed
carmo-lucas <msclucaslima@gmail.com>
2022-06-27 22:01:50 -0300

    Changes formatting and removed unused functions

--- NFCE/body.py
+++ NFCE/body.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from NFCE import header
 
 def read_raw_table(url):
 
@@ -19,39 +20,21 @@ def clean_strings(tbl):
 
 def separate_columns(tbl):
     # Separa os valores em colunas
-
+    # pat='[ ]{2,}' significa dois ou mais espaços
     tbl[['nome', 'qtde', 'unidade', 'vl_unit']] = tbl[0].str.split(pat='[ ]{2,}', expand=True)
 
     # Remove colunas que já foram transformadas
-
-    # pat='[ ]{2,}' significa dois ou mais espaços
-
     tbl = tbl.drop(columns=[0, 1])
 
     return tbl
 
-def parse_doubles(tbl):    # Troca vírgulas por pontos para futura conversão para valores numéricos
+def parse_doubles(tbl, meta):
+# TODO: Pensar em como melhorar essa função parse_doubles para ser um pouco mais robusta, remover os dois argumentos
+    
+    # Troca vírgulas por pontos para futura conversão para valores numéricos
     tbl = tbl.replace(",", ".", regex=True)
     # Remove espaços das células e converte para numérico
     tbl[['qtde', 'vl_unit', 'vl_total']] = tbl[['qtde', 'vl_unit','vl_total']].replace(" ", "", regex=True).astype('float64')
-    tbl[['conta_despesa', 'data_transacao']] = conta_despesa, data_transacao
+    tbl[['ID']] = meta["chave_acesso"] # isso não é uma boa ideia
     return tbl
 
-def clean_raw_data(tbl):
-    tbl = clean_strings(tbl)
-    tbl = separate_columns(tbl)
-    tbl = parse_doubles(tbl)
-    return tbl
-
-def generate_items_table(url):
-    page          = request_nfce_page(url)
-    conta_despesa = fetch_expense_account(page)
-    cnpj          = fetch_cnpj(page)
-    tbl           = read_raw_table(url)
-    tbl           = clean_raw_data(tbl)
-    
-    return tbl
-
-tabela = generate_items_table(url)
-
-print(tabela)
